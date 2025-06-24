f9e5345eb65a42ae55e2cbb9bfcd9341cd02356b
Abdulrahim Jamil <jameelangoabdulraheem@gmail.com>
2025-06-24 21:52:44 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require("express");
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`app is listening on port: ${PORT}`));
