95699ea6a05095362b609e6fd621e30d302bcc79
Abdulrahim Jamil <jameelangoabdulraheem@gmail.com>
2025-06-25 21:16:46 +0100

    chore: added passport config

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 require('dotenv').config();
 const express = require("express");
+const session = require("express-session");
+const { passport } = require("./auth/passport-config");
 
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
+app.use(session({
+    secret: "dattebayo!",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: err.message });
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`app is listening on port: ${PORT}`));
---
+++ auth/passport-config.js
@@ -0,0 +1,58 @@
+const password = require("passport");
+const db = require("../models/queries");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+
+
+password.use(new LocalStrategy( async(username, password, done) => {
+    try{
+        const user = await db.getUserByName(username);
+        if(!user){
+            return done(null, false, { message: "Username not found!"});
+        }
+
+        const match = await bcrypt.compare(password, user.password);
+        if(!match){
+            return done(null, false, { message: "Password is incorrect!"});
+        }
+
+        return done(null, user);
+    }catch(err){
+        return done(err);
+    }
+}));
+
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try{
+        const user = await db.getUserById(id);
+        done(null, user);
+    }catch(err){
+        done(err);
+    }
+});
+
+function handleLogin(req, res, next){
+    passport.authenticate("local", (err, user, info) => {
+        if(err) return next(err);
+        if(!user){
+            return res.status(401).json({ message: info?.message || "Invalid username or password"});
+        }
+
+        req.login(user, (err) => {
+            if(err) return next(err);
+            return res.status(200).json({ message: "login successful", user: user });
+        });
+    })(req, res, next);
+}
+
+
+module.exports = {
+    passport,
+    handleLogin
+}
